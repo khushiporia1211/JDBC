2400297aae3941df0f4ed7638d6c9f6ac02ceaa7
khushi <khushiporia09@gmail.com>
2026-04-28 09:24:19 +0530

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,7 @@ import java.sql.*;
 public class Main{
     private static final String url = "jdbc:mysql://127.0.0.1:3306/mydb";
     private static final String username="root";
-    private static final String password="k2H@ushi12";
+    private static final String password="your password";
     public static void main(String[] args)  {
        try{
            Class.forName("com.mysql.cj.jdbc.Driver");
