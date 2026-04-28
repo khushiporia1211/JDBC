23c6648e0c29a3ae429f0be96351e23e97e52d5c
khushi <khushiporia09@gmail.com>
2026-04-28 09:28:03 +0530

    Update updateUsingPreStmnt.java

--- src/updateUsingPreStmnt.java
+++ src/updateUsingPreStmnt.java
@@ -3,7 +3,7 @@ import java.sql.*;
 public class updateUsingPreStmnt {
     private static final String url = "jdbc:mysql://127.0.0.1:3306/mydb";
     private static final String username="root";
-    private static final String password="k2H@ushi12";
+    private static final String password="your paasword";
     public static void main(String[] args)  {
         try{
             Class.forName("com.mysql.cj.jdbc.Driver");
